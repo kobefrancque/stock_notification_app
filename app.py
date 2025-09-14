b15bb7dcb650ebb243e915884c5d4886142a54e4
kobefrancque <kobefrancqueprive@hotmail.com>
2025-09-14 11:04:06 +0200

    Add login page + navbar link to the page

--- app.py
+++ app.py
@@ -20,6 +20,10 @@ class Users():
 def index():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  
