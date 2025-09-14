90172d9350cead48ee81b54209304b54c59391b9
kobefrancque <kobefrancqueprive@hotmail.com>
2025-09-14 14:01:10 +0200

    Add database entry logic for new registered user

--- app.py
+++ app.py
@@ -36,7 +36,23 @@ def registration():
     if request.method == 'GET':
         return render_template('registration.html', registration_form=registration_form)
     elif request.method == 'POST':
-        return redirect(url_for('index'))
+        if registration_form.validate_on_submit():
+            username = registration_form.username.data
+            password = registration_form.password.data
+            phone = registration_form.phone.data
+            sp500_enabled = registration_form.sp500_enabled.data
+
+            new_user = Users(
+                username=username,
+                password=password,
+                phone_number=phone,
+                sp500_alert=sp500_enabled
+            )
+
+            db.session.add(new_user)
+            db.session.commit()
+
+            return redirect(url_for('index'))
 
 if __name__ == '__main__':
     with app.app_context():
