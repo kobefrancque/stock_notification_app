41e23b2ddfd8c0ad072c377ae0243912e28bc099
kobefrancque <kobefrancqueprive@hotmail.com>
2025-09-14 11:12:25 +0200

    Update registration form to include correct fields

--- forms.py
+++ forms.py
@@ -1,7 +1,13 @@
 from flask_wtf import FlaskForm
-
-class LoginForm(FlaskForm):
-    pass
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Length, Regexp
 
 class RegistrationForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired(), Length(min=8, max=20)])
+    phone = StringField('phone', validators=[DataRequired(), Regexp(r'^\d{10}$', message="Phone number must be 10 digits.")])
+    sp500_enabled = BooleanField('SP500 alerts', default=False)
+    submit = SubmitField('register')
+
+class LoginForm(FlaskForm):
     pass
